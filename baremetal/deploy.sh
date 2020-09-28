221088db12578aecd21d4e2918c019111addd19c
hsin <13961132+opsxin@users.noreply.github.com>
2020-09-28 17:34:13 +0800

    Update (#5)
    * Baremetal: Update
    
    * Docker: 添加占位文件
    
    如果不添加，docker hub 构建 image 会失败
    
    * Compose: Update docker-compose
    
    * Ansible: update molecule

--- baremetal/deploy.sh
+++ baremetal/deploy.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-centos_source() {
-    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-    if [ "${os_version}" -eq 7 ]; then
-        curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
-    elif [ "${os_version}" -eq 8 ]; then
-        curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo
+set -u
+
+ensure_group_exist() {
+    egrep "^${running_group}" /etc/group >& /dev/null
+    if [ $? -ne 0 ]; then
+        echo "添加组: ${running_group}"
+        groupadd ${running_group}
     fi
-    yum makecache && yum update -y
 }
 
 ensure_user_exist() {
-    egrep "^${running_user}" /etc/group >& /dev/null
-    if [ $? -ne 0 ]; then
-        groupadd ${running_user}
-    fi
-
     egrep "^${running_user}" /etc/passwd >& /dev/null
     if [ $? -ne 0 ]; then
-        useradd -g ${running_user} ${running_user}
+        echo "添加用户: ${running_user}"
+        useradd -s /usr/sbin/nologin -g ${running_group} ${running_user}
     fi
 }
 
+centos_source() {
+    echo "CentOS: 修改国内 yum 源"
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+    if [ "${os_version}" -eq 7 ]; then
+        curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
+    else
+        curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo
+    fi
+    yum makecache
+}
+
 install_nginx() {
+    echo "开始安装 Nginx"
     if [ "${os}" == "centos" ]; then
         cat > /etc/yum.repos.d/nginx.repo << 'EOF'
 [nginx-stable]
@@ -42,30 +50,28 @@ EOF
         add-apt-repository "deb http://mirrors.ustc.edu.cn/nginx/ubuntu/ $(grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2) nginx"
         apt update && apt install -y nginx 
     fi
-    sed -i -e '$iclient_max_body_size 1024M;' \
-        -e "s/user  nginx;/user ${running_user} ${running_user};/" /etc/nginx/nginx.conf
-    cat > /etc/nginx/conf.d/default.conf << 'EOF'
+    echo "修改 nginx 运行用户和组"
+    sed -e '$iclient_max_body_size 1024M;' \
+        -e "s/user  nginx;/user ${running_user} ${running_group};/" \
+        -i /etc/nginx/nginx.conf
+    echo "添加 nginx 配置"
+    mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+    cat > /etc/nginx/conf.d/edusoho.conf << 'EOF'
 server {
     listen 80;
-
     server_name _;
-
     # 程序的安装路径
     root /var/www/edusoho/web;
-
     # 日志路径
     access_log /var/log/nginx/example.com.access.log;
     error_log /var/log/nginx/example.com.error.log;
-
     location / {
         index app.php;
         try_files $uri @rewriteapp;
     }
-
     location @rewriteapp {
         rewrite ^(.*)$ /app.php/$1 last;
     }
-
     location ~ ^/udisk {
         internal;
         root /var/www/edusoho/app/data/;
@@ -89,22 +95,18 @@ server {
         
         # 关闭日志记录
         access_log off;
-
         # 关闭gzip压缩，减少CPU消耗，因为图片的压缩率不高。
         gzip off;
     }
-
     # 配置css/js文件
     location ~* \.(css|js)$ {
         access_log off;
         expires 3y;
     }
-
     # 禁止用户上传目录下所有.php文件的访问，提高安全性
     location ~ ^/files/.*\.(php|php7.0)$ {
         deny all;
     }
-
     # 以下配置允许运行.php的程序，方便于其他第三方系统的集成。
     location ~ \.php$ {
         # [改] 请根据实际php-fpm运行的方式修改
@@ -121,10 +123,11 @@ EOF
 }
 
 install_php() {
+    echo "开始安装 PHP 7.1"
     if [ "${os}" == "centos" ]; then
         if [ "${os_version}" -eq 7 ]; then
             yum install -y https://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/remi-release-7.rpm
-        elif [ "${os_version}" -eq 8 ]; then
+        else
             yum install -y https://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/remi-release-8.rpm
         fi
         sed -e 's!^metalink=!#metalink=!g' \
@@ -147,8 +150,9 @@ post_max_size = 1024M
 memory_limit = 1024M
 upload_max_filesize = 1024M
 EOF
-        sed -i -e "s/apache/${running_user}/g" -e "s/^listen = .*/listen = 127.0.0.1:9000/" \
-            /etc/opt/remi/php71/php-fpm.d/www.conf
+        sed -e "s/apache/${running_user}/g" \
+            -e "s/^listen = .*/listen = 127.0.0.1:9000/" \
+            -i /etc/opt/remi/php71/php-fpm.d/www.conf
         systemctl restart php71-php-fpm
     else
         export DEBIAN_FRONTEND=noninteractive
@@ -171,22 +175,29 @@ EOF
 }
 
 install_mysql() {
-    if [ "${os}" == "centos" -a "${os_version}" -eq 7 ]; then
-        yum install -y https://mirrors.ustc.edu.cn/mysql-repo/mysql57-community-release-el7-9.noarch.rpm
-        yum makecache && yum install -y mysql-community-server mysql-community-client
-    elif [ "${os}" == "centos" -a "${os_version}" -eq 8 ]; then
-        yum install -y mysql mysql-server
-    elif [[ "${os}" == "ubuntu" && "${os_version}" -lt 2004 ]]; then
-        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8C718D3B5072E1F5
-        add-apt-repository "deb https://mirrors.tuna.tsinghua.edu.cn/mysql/apt/ubuntu/ $(grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2) mysql-5.7"
-        apt update && apt install -y mysql-server mysql-client
-    elif [[ "${os}" == "ubuntu" && "${os_version}" -ge 2004 ]]; then
-        apt install -y mysql-server-8.0 mysql-client-8.0
+    echo "开始安装 MySQL"
+    if [ "${os}" == "centos" ]; then
+        if [ "${os_version}" -eq 7 ]; then
+            yum install -y https://mirrors.ustc.edu.cn/mysql-repo/mysql57-community-release-el7-9.noarch.rpm
+            yum makecache && yum install -y mysql-community-server mysql-community-client
+        else
+            yum install -y mysql mysql-server
+        fi
+        systemctl restart mysqld
+    else
+        if [ "${os_version}" -lt 2004 ]; then
+            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8C718D3B5072E1F5
+            add-apt-repository "deb https://mirrors.tuna.tsinghua.edu.cn/mysql/apt/ubuntu/ $(grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2) mysql-5.7"
+            apt update && apt install -y mysql-server mysql-client
+        else
+            apt install -y mysql-server-8.0 mysql-client-8.0
+        fi
+        systemctl restart mysql
     fi
-    systemctl restart mysql
 }
 
 install_edusoho() {
+    echo "开始下载 web 文件"
     if [ ! -d "/var/www" ]; then
         mkdir "/var/www"
     fi
@@ -196,7 +207,7 @@ install_edusoho() {
     yum install -y unzip || apt install -y unzip
     curl http://download.edusoho.com/edusoho-8.7.15.zip -o /tmp/edusoho.zip
     unzip /tmp/edusoho.zip -d "/var/www/"
-    chown -R ${running_user}:${running_user} "/var/www/edusoho"
+    chown -R ${running_user}:${running_group} "/var/www/edusoho"
 }
 
 if [ $(whoami) != "root" ]; then
@@ -206,21 +217,26 @@ fi
 
 if grep -qs "ubuntu" /etc/os-release; then
 	os="ubuntu"
-	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+	os_version="$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')"
+	running_group="www-data"
 	running_user="www-data"
-elif [[ -e /etc/centos-release ]]; then
+elif [ -e /etc/centos-release ]; then
 	os="centos"
-	os_version=$(grep -oE '[0-9]+' /etc/centos-release | head -1)
-	running_user="www-data"
+	os_version="$(grep -oE '[0-9]+' /etc/centos-release | head -1)"
+	running_group="nobody"
+	running_user="nobody"
 else
 	echo "暂时只支持 Ubuntu 16.04,18.04,20.04 和 CentOS 7,8。"
 	exit 1
 fi
 
-[ "${os}" == "centos" ] && centos_source
+## 修改为国内源，加速下载
+#[ "${os}" == "centos" ] && centos_source
 
+ensure_group_exist
 ensure_user_exist
 install_nginx
 install_php
 install_mysql
 install_edusoho
+
